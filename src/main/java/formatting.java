0c7b6cafea67e1f38f0b2464dd25f94179d8a848
Yevgeny Carranza <lijiu001@gmail.com>
2022-03-16 11:21:13 -0400

    commit

--- src/main/java/formatting.java
+++ src/main/java/formatting.java
@@ -5,14 +5,14 @@ public class formatting {
         int qty = 20;
         float price = 5.0f;
         float total = qty * price;
-        String s = String.format("%-15s %5s %10s\n", "Item", "Qty", "Price");
-        String s1 = String.format("%-15s %5s %10s\n", "----", "---", "-----");
+        String s = String.format("%-15s %5s %10s %15s\n" , "Item", "Qty", "Price", "Total");
+        String s1 = String.format("%-15s %5s %10s %15s\n", "----", "---", "-----", "-----");
         String output = s + s1;
         System.out.println(output);
-        String line = String.format("%-15s %5d %10.2f\n", productName, qty, price);
+        String line = String.format("%-15s %5d %10.2f %15.2f", productName, qty, price, total);
         System.out.println(line);
-        String grandTotal = String.format("%-15s %5s %10.2f\n", "Total", " ", total);
-        System.out.println(grandTotal);
+
+
 
 
 
